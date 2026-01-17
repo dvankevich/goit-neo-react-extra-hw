00f2a496598e729af397af9805557bb1109bd6c4
dvankevich <dvankevich@gmail.com>
2026-01-17 18:47:21 +0200

    fix input colors

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ export const getTheme = (mode) =>
   createTheme({
     cssVariables: true,
     palette: {
-      mode, // "light" або "dark"
+      mode,
       primary: {
         main: "#556cd6",
       },
@@ -21,10 +21,25 @@ export const getTheme = (mode) =>
         styleOverrides: {
           root: {
             backgroundColor: mode === "light" ? "#ffffff" : "#2d2d2d",
+          },
+          input: {
+            color: mode === "light" ? "rgba(0, 0, 0, 0.87)" : "#ffffff",
+
+            "&::placeholder": {
+              color:
+                mode === "light"
+                  ? "rgba(0, 0, 0, 0.42)"
+                  : "rgba(255, 255, 255, 0.5)",
+              opacity: 1,
+            },
+
             "&:-webkit-autofill": {
-              WebkitBoxShadow: `0 0 0 100px ${mode === "light" ? "#ffffff" : "#2d2d2d"} inset !important`,
+              WebkitBoxShadow: `0 0 0 100px ${
+                mode === "light" ? "#ffffff" : "#2d2d2d"
+              } inset !important`,
               WebkitTextFillColor:
                 mode === "light" ? "#000000" : "#ffffff !important",
+              caretColor: mode === "light" ? "#000000" : "#ffffff",
             },
           },
         },
